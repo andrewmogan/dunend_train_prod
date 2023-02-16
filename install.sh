38cd4fa2bae6e00bb4ead5cc248fc797ed418e0a
Kazuhiro Terao <kterao@sdfiana007.sdf.slac.stanford.edu>
2023-02-16 15:00:57 -0800

    path fix

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ then
     echo "Failed to install larpix_readout_parser"
     exit 1
 fi
-cd ..
+cd -
 
 echo "installing SuperaAtomic"
 cd modules/SuperaAtomic
@@ -22,7 +22,7 @@ then
     echo "Failed to install SuperaAtomic"
     exit 1
 fi
-cd ..
+cd -
 
 echo "installing edep2supera"
 cd modules/edep2supera
@@ -32,7 +32,7 @@ then
     echo "Failed to install edep2supera"
     exit 1
 fi
-cd ..
+cd -
 
 echo "installing larnd2supera"
 cd modules/larnd2supera
@@ -42,6 +42,6 @@ then
     echo "Failed to install larnd2supera"
     exit 1
 fi
-cd ..
+cd -
 
 echo "done"
